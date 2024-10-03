b386c018bd76a97fb805a106ee7444b2538a3ab8
FazliddinAbdazimov <abdazimovfazliddin27@gmail.com>
2024-10-03 21:05:34 +0500

    Initial Commit

---
+++ SwiftUI-WeatherTests/SwiftUI_WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherTests.swift
+//  SwiftUI-WeatherTests
+//
+//  Created by Fazliddin Abdazimov on 03/10/24.
+//
+
+import Testing
+@testable import SwiftUI_Weather
+
+struct SwiftUI_WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
